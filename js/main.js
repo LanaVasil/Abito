2747208b103546c46176307a734705bd6bef47cd
LanaVasil <88096819+LanaVasil@users.noreply.github.com>
2021-07-29 16:37:03 +0300

    Адаптивная верстка сайта Авито

--- js/main.js
+++ js/main.js
@@ -13,3 +13,13 @@ var swiper2 = new Swiper(".mySwiper2", {
     swiper: swiper,
   },
 });
+
+const menuBtn = document.querySelector(".menu-button");
+const menu = document.querySelector(".navbar-panel");
+const menuCloseBtn = document.querySelector(".close-menu");
+menuBtn.addEventListener("click", () => {
+  menu.classList.toggle("open");
+})
+menuCloseBtn.addEventListener("click", () => {
+  menu.classList.toggle("open");
+})
